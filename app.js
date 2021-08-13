c886a1c4c4c1d55253fa4659d8e52f3aa83db91d
Zach Marmolejo <zachary.marmolejo@protonmail.com>
2021-08-13 10:49:23 -0500

    JS Mail Server

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+// Mailchimp module
+const mailchimp = require('@mailchimp/mailchimp_marketing')
+
 // Mail Server 
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const port = 3000
+const port = process.env.PORT || 3000
+
+//Api Key 
+import {mail_api_key} from './api_key.js';
+import {mail_listId} from './api_key.js';
 
 const app = express();
 app.listen(port, function(){
@@ -20,10 +27,52 @@ app.get('/', function(req,res){
     res.sendFile(__dirname + "/signup.html");
 });
 
+
+mailchimp.setConfig({
+    apiKey: mail_api_key,
+    server: "us5"
+});
+
+
 app.post('/', function(req,res){
+    // data submitted by user
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const email = req.body.email;
 
     console.log(firstName, lastName, email);
-})
+
+    const listId = mail_listId;
+
+    // Creating object with users data
+    const subscribingUser = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email
+    };
+
+    //Upload data to the Server
+    async function run() {
+        const response = await mailchimp.lists.addListMember(listId,{
+            email_address: subscribingUser.email,
+            status:"subscribed",
+            merge_fields: {
+                FNAME: subscribingUser.firstName,
+                LNAME: subscribingUser.lastName
+            }
+        });
+        // after logging contacts listId, redirect to success page
+        res.sendFile(__dirname + "/success.html")
+        console.log(`Added contact as an audience member. The contact's id is ${response.id}.`);
+    }
+
+    // running function and catching errors
+    run().catch(e => res.sendFile(__dirname + "/failure.html"));
+    
+});
+
+// failure redirection to home page
+app.post('/failure', function(req,res){
+    res.redirect("/");
+});
+
